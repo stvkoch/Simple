b21cddf6348e6d2caf4bba6acc0063e944ef8876
Steven Koch <steven.koch@co.sapo.pt>
2013-05-21 01:19:37 +0100

    make backbone light

--- src/Simple/Application/Backbone.php
+++ src/Simple/Application/Backbone.php
@@ -4,92 +4,63 @@ namespace Simple\Application;
 
 class Backbone {
 
-	public $request;
-	public $response;
-	public $resources;
+	protected $_resources = array();
 
 
-
-	public function __construct(\Simple\Request\Base $request=null, $response=null, $resource=null, $bootstrap=array())
+	public function __construct($bootstrap=array())
 	{
-		$this->request=$request;
-		$this->response=$response;
-		$this->resources=$resource;
 		foreach ($bootstrap as $callback) {
 			$callback($this);
 		}
 	}
 
-	public function setRequest($request)
+
+	public function setResource($id, &$resource)
 	{
-		$this->request=$request;
+		$this->_resources[$id]=$resource;
 	}
 
-	public function getRequest()
+	public function getResourceById($id)
 	{
-		return $this->request;
+		return $this->_resources[$id];
 	}
 
-	public function setResponse($response)
-	{
-		$this->response=$response;
-	}
-
-	public function getResponse()
-	{
-		return $this->response;
-	}
-
-	public function setResource($resource)
-	{
-		$this->resources=$resource;
-	}
-
-	public function getResource()
-	{
-		return $this->resources;
-	}
-
-//	public function hasClass
-
-	public function run()
-	{
-		$this->runResources($this->resources);
-	}
 
 	public function runResources($resources)
 	{
-		try {
-			foreach ($resources as &$resource) {
-				$className = $resource['class'] = $resource['namespace'].'\\'.ucfirst($resource['class']);
 
-				if(class_exists($className)) {
-					$obj = new $className();
-				} else {
-					throw new \Exception("notFoundClassException $className", 404);
+			foreach ($resources as &$resource) {
+
+				if(!isset($resource['id']))
+					$resource['id'] = $resource['namespace'].'\\'.ucfirst($resource['class']);
+
+				if(!isset($this->_resources[$resource['id']]))
+				{
+					$className = $resource['class'] = $resource['namespace'].'\\'.ucfirst($resource['class']);
+					if(class_exists($className))
+					{
+						$this->_resources[$resource['id']] = new $className();
+					}
+					else
+					{
+						throw new \Exception("notFoundClassException $className", 404);
+					}
 				}
+
 				$functionName = (isset($resource['function'])?$resource['function']:$resource['action'].'Action');
 
-				if(!method_exists($obj, $functionName))
+				if(!method_exists($this->_resources[$resource['id']], $functionName))
 					throw new \Exception("notFoundFunctionException $functionName", 404);
 
 				//extended from Simple\Middleware\Base
-				if(method_exists($obj, 'setBackbone')) {
-					call_user_func_array(array($obj, 'setBackbone'), array(&$this));
-				}
-				if(method_exists($obj, 'setCurrentResource')) {
-					call_user_func_array(array($obj, 'setCurrentResource'), array($resource));
-				}
+				if(method_exists($this->_resources[$resource['id']], 'setBackbone'))
+					call_user_func_array(array($this->_resources[$resource['id']], 'setBackbone'), array(&$this));
 
-				if(!call_user_func(array($obj, $functionName)))
+				if(method_exists($this->_resources[$resource['id']], 'setResource'))
+					call_user_func_array(array($this->_resources[$resource['id']], 'setResource'), array($resource));
+
+				if(call_user_func(array($this->_resources[$resource['id']], $functionName))===false)
 					break;
 			}
-		} catch (\Exception $e) {
-var_dump($e->getMessage(), $resource);exit;
-			if($e->getCode()==404)
-				\Simple\Response\HTTP::redirect( $this->request->getURL().'/404', 404 );
-			else
-				\Simple\Response\HTTP::redirect( $this->request->getURL().'/500', 500 );
-		}
 	}
 }
