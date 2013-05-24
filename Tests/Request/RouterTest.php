e484d35f6d8f65ea94d96150cae8e56eed4c29e8
stvkoch <stvkoch@gmail.com>
2013-05-24 15:02:31 +0100

    create test diff arrays

--- Tests/Request/RouterTest.php
+++ Tests/Request/RouterTest.php
@@ -2,95 +2,84 @@
 
 class RouterTest extends PHPUnit_Framework_TestCase
 {
-	protected function setUp()
+    protected function setUp()
     {
-    	\Simple\Config\PHP::setPath(__DIR__.'/config');
+        \Simple\Config\PHP::setPath(__DIR__.'/config');
 
-    	$_SERVER['SERVER_NAME'] = 'phpunit.test';
+        $_SERVER['SERVER_NAME'] = 'phpunit.test';
     }
 
     public function testDisableWarning() {
-    	$this->assertEquals(true, true);
+        $this->assertEquals(true, true);
     }
 
-	public function testMiddlewares()
-	{
-		$_SERVER['REQUEST_URI'] = '/admin/controller/function';
-		$_SERVER['REQUEST_METHOD'] = 'TEST';
-		$_SERVER['QUERY_STRING'] = 'foo=1&bar=2';
-		$request = new \Simple\Request\HTTP( $_SERVER, $_REQUEST, $_FILES );
+    public function testMiddlewares()
+    {
+        $_SERVER['REQUEST_URI'] = '/admin/controller/function';
+        $_SERVER['REQUEST_METHOD'] = 'TEST';
+        $_SERVER['QUERY_STRING'] = 'foo=1&bar=2';
+        $request = new \Simple\Request\HTTP( $_SERVER, $_REQUEST, $_FILES );
 
-		$routes = \Simple\Config\PHP::getScope('routesesource');
+        $routes = \Simple\Config\PHP::getScope('routesesource');
 
-		$router = new \Simple\Request\Router( $routes );
+        $router = new \Simple\Request\Router( $routes );
 
-		$resources = array(
+        $resources = array(
 
-			array(
-				'namespace' => '\Simple\Middleware\View',
-				'class'=>'Cache',
-				'function'=>'open',
-				'id'=>'Simple.middleware.view.cache.open'
-			),
+            array(
+                'namespace' => '\Simple\Middleware\View',
+                'class'=>'Cache',
+                'function'=>'open',
+                'id'=>'Simple.middleware.view.cache.open'
+            ),
 
-  			array(
-				'namespace' => '\Frontend\Middleware',
-				'class'=>'Flash',
-				'function'=>'openFlash',
-				'id'=>'front.mid.flash.open'
-			),
+            array(
+                'namespace' => '\Frontend\Middleware',
+                'class'=>'Flash',
+                'function'=>'openFlash',
+                'id'=>'front.mid.flash.open'
+            ),
 
-    	);
+        );
 
-		$resourcesFromRoute = $router->getResourcesByRequest($request);
+        $resourcesFromRoute = $router->getResourcesByRequest($request);
+        $arrayDiff = $this->_array_diff_multidimensional($resources, $resourcesFromRoute);
+        $this->assertTrue(empty($arrayDiff));
 
-		$this->assertEmpty(array_diff_assoc($resources, $resourcesFromRoute));
+    }
 
-	}
-/*
-	public function testRouter2()
-	{
-		$_SERVER['REQUEST_URI'] = '/controllerName/show';
-		$_SERVER['REQUEST_METHOD'] = 'TEST';
-		$_SERVER['QUERY_STRING'] = 'foo=1&bar=2';
-		$request = new \Simple\Request\HTTP( $_SERVER, $_REQUEST, $_FILES );
 
-		$routes = \Simple\Config\PHP::getScope('routes');
-		$router = new \Simple\Request\Router( $routes );
+    //http://stackoverflow.com/questions/6026431/php-mutidimensional-array-diff
+    private function _array_diff_multidimensional($arr1, $arr2) {
+        $answer = array();
+        foreach($arr1 as $k1 => $v1) {
+            // is the key present in the second array?
+            if (!array_key_exists($k1, $arr2)) {
+               $answer[$k1] = $v1;
+               continue;
+            }
 
-		$resource = array(
-			"module"=>"Frontend",
-			"controller"=>"controllerName",
-			"action"=>"show",
-			"format"=>"html",
-			"params"=>array()
-		);
-		$resourceFromRoute = $router->getResourceByRequest($request);
-		$this->assertEquals($resource, $resourceFromRoute);
+            // PHP makes all arrays into string "Array", so if both items
+            // are arrays, recursively test them before the string check
+            if (is_array($v1) && is_array($arr2[$k1])) {
+                $answer[$k1] = $this->_array_diff_multidimensional($v1, $arr2[$k1]);
+                continue;
+            }
 
-	}
+            // do the array_diff string check
+            if ((string)$arr1[$k1] === (string)$arr2[$k1]) {
+                continue;
+            }
 
-	public function testResourceJson()
-	{
-		$_SERVER['REQUEST_URI'] = '/xpto/okidoki.json';
-		$_SERVER['REQUEST_METHOD'] = 'TEST';
-		$_SERVER['QUERY_STRING'] = 'foo=1&bar=2';
-		$request = new \Simple\Request\HTTP( $_SERVER, $_REQUEST, $_FILES );
+            // since both values are not arrays, and they don't match,
+            // simply add the $arr1 value to match the behavior of array_diff
+            // in the PHP core
+            $answer[$k1] = $v1;
+        }
 
-		$routes = \Simple\Config\PHP::getScope('routes');
-		$router = new \Simple\Request\Router( $routes );
+        // done!
+        return array_filter($answer);
+    }
 
-		$resource = array(
-			"module"=>"Frontend",
-			"controller"=>"xpto",
-			"action"=>"okidoki",
-			"format"=> "json",
-			"params"=>array()
-		);
 
-		$resourceFromRoute = $router->getResourceByRequest($request);
-
-		$this->assertEquals($resource, $resourceFromRoute);
-	}
-*/
 }
